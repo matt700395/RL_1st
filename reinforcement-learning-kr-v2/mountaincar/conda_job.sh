3ad2d86c7bb7e2f5e80672f5bc197eb6409d1937
matt700395 <matt7003@korea.ac.kr>
2021-11-21 14:05:10 +0900

    mountain_car

---
+++ reinforcement-learning-kr-v2/mountaincar/conda_job.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash
+#PBS -l nodes=1:gpus=1:ppn=1
+
+source activate rl_env
+
+cd ~/PycharmProjects/RL_1st/reinforcement-learning-kr-v2/mountaincar
+
+python DQN_train.py
+
+conda deactivate
+
